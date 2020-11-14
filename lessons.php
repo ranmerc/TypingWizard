e8630109411419940b2679289ccdec8c4da95e92
Anish Nair <nairanish2001@gmail.com>
2020-11-14 11:58:09 +0530

    Lessons.php added

---
+++ lessons.php
@@ -0,0 +1,37 @@
+<?php
+    include("header.html");
+    
+?>
+<link rel="stylesheet" href="Styles/lessons.css">
+
+
+<div id="sidebar" class="row">
+  <div class="lesson_title_div column1">
+      <p class="lesson_title" data-lesson="1">Lesson 1</p>
+      <p class="lesson_title" data-lesson="2">Lesson 2</p>
+      <p class="lesson_title" data-lesson="3">Lesson 3</p>
+      <p class="lesson_title" data-lesson="4">Lesson 4</p>
+      <p class="lesson_title" data-lesson="5">Lesson 5</p>
+      <p class="lesson_title" data-lesson="6">Lesson 6</p>
+      <p class="lesson_title" data-lesson="7">Lesson 7</p>
+      <p class="lesson_title" data-lesson="8">Lesson 8</p>
+      <p class="lesson_title" data-lesson="9">Lesson 9</p>
+      <p class="lesson_title" data-lesson="10">Lesson 10</p>
+      <p class="lesson_title" data-lesson="11">Lesson 11</p>
+      <p class="lesson_title" data-lesson="12">Lesson 12</p>
+      <p class="lesson_title" data-lesson="13">Lesson 13</p>
+      <p class="lesson_title" data-lesson="14">Lesson 14</p>
+      <p class="lesson_title" data-lesson="15">Lesson 15</p>
+  </div>
+  <div class="lesson_content_div  hidden column2" >
+      
+  </div>
+</div>
+<div id="spaceforkeyboard">
+  <p>sdfsdfsdf</p>
+</div>
+
+<script src="Js/lessons.js"></script>
+<?php
+  include("footer.html");
+?>
