67b7826f1e1f447ab3c4205e5f47c979b55be467
Kamran Ansari <crossthinking786@gmail.com>
2020-11-28 11:20:58 +0530

    Challenge completed

--- Js/challenge.js
+++ Js/challenge.js
@@ -2,10 +2,16 @@ var para = document.querySelector("#para");
 var dropdown = document.querySelector("#dropdown");
 var title = document.querySelector("#challengeName");
 var input = document.querySelector("#challengeInput"); 
+var modal = document.querySelector("#modal");
+var options = Array.from(document.querySelectorAll("option")).map(function(o){ return o.value});
 var count = 0;
 var tokens;
 var startTime;
 var endTime;
+
+document.querySelector(".close").addEventListener("click", function(){
+    window.location.reload();
+});
 
 function insertPara(name, value){
     para.innerHTML = '';
@@ -27,6 +33,7 @@ xhr.send();
 xhr.onload = function() {
     console.log(xhr.responseText);
     obj = JSON.parse(xhr.responseText);
+    dropdown.selectedIndex = Math.floor(Math.random() * options.length);
     insertPara(dropdown.value, obj[dropdown.value]);
 }
 
@@ -40,6 +47,16 @@ input.addEventListener("blur", clearInput);
 function clearInput(){
     input.value = '';
     input.removeEventListener("keydown", checker);
+    count = 0;
+    clear("wrong");
+    clear("right");
+}
+
+function clear(className){
+    var wrongs = document.querySelectorAll("." + className);
+    for(var i = 0; i < wrongs.length; ++i){
+      wrongs[i].classList.remove(className);
+    }
 }
 
 function toggler(){
@@ -55,7 +72,7 @@ function checker(e){
         tokens[count].classList.remove("right");
         return;
     }
-    if(!(/^[A-Za-z0-9"\'\:,\s]$/.test(e.key)) || e.ctrlKey){
+    if(!(/^[A-Za-z0-9"\.\'\:\-\,\s]$/.test(e.key)) || e.ctrlKey){
         e.preventDefault();
         return;
     }
@@ -65,6 +82,19 @@ function checker(e){
     else
         tokens[count].classList.add("right");
     ++count;
-    // if(count == tokens.length)
-        
+    if(count == tokens.length){
+        endTime = new Date;
+        var minutes = (endTime - startTime)/60000;
+        var wpm = (tokens.length/(5*minutes)).toFixed(2);
+        var seconds = (minutes.toFixed(2) + "").split(".")[1];
+        minutes = (minutes + "").split(".")[0];
+        var errors = document.querySelectorAll(".wrong").length;
+        console.log("wpm " + wpm + " minutes "+ minutes +" seconds " + seconds + " errors " + errors);
+        document.querySelector("#minutes").innerHTML = minutes;
+        document.querySelector("#seconds").innerHTML = seconds;
+        document.querySelector("#wpm .modalValue").innerHTML = wpm;
+        document.querySelector("#errors .modalValue").innerHTML = errors;
+        modal.style.display = "flex";
+        document.querySelector(".modalBlur").style.display = "inline";
+    }
 }
